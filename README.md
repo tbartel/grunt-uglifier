a1267b2648c950faaf1434e5baaf7e59a409faf0
Tyler Bartel <tyler@packetpower.com>
2014-08-25 11:26:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# grunt-uglifier
+
+> Combine and obfuscate Node.js applications using node-uglifier
+
+## Uglifier task
+_Run this task with the `grunt uglifier` command._
+
+## Usage Examples
+```
+uglifier: {
+    obfuscate: {
+        mainFile: 'src/main.js',
+        destFile: 'dest/main.js'
+    }
+}
+```
---
+++ tasks/uglifier.js
@@ -0,0 +1,35 @@
+/*
+ * grunt-contrib-uglifier
+ * Uglify and obfuscate full Node.js projects
+ */
+
+module.exports = function(grunt){
+    'use strict';
+
+    var fs = require('fs');
+    var path = require('path');
+    var NodeUglifier = require('node-uglifier');
+
+    grunt.registerMultiTask('uglifier', 'Uglify Node.js soure code.', function(){
+        var options = this.options({
+            randomSeed: (this.options.randomSeed !== undefined) 
+                            ? this.options.randomSeed
+                            : generateSeed()
+        });
+
+        var mainFile = this.data.mainFile;
+        var destFile = this.data.destFile;
+
+        var obfuscatedSource = new NodeUglifier(mainFile, {
+            rngSeed: options.randomSeed
+        });
+
+        obfuscatedSource = obfuscatedSource.merge().uglify();
+        obfuscatedSource.exportToFile(destFile);
+    });
+
+    function generateSeed(){
+        return Math.floor((Math.sin(Math.random()).toString().substr(6)) * 1000);
+    }
+
+};
